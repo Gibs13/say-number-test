b89325c1773c5c0b2e333fd879f3c23f4cae8799
Gibs13 <Gsgwlol@gmail.com>
2017-06-19 13:50:24 +0200

    added nodejs module and code for google home console

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const ActionsSdkApp = require('actions-on-google').ActionsSdkApp;
+
+exports.<insertCloudFunctionName>= (req, res) => {
+  const app = new ActionsSdkApp({request: req, response: res});
+
+  // Create functions to handle requests here
+
+}
