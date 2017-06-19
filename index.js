eb876f4fac6aee4c82695ba24df170e376154fa0
Gibs13 <Gsgwlol@gmail.com>
2017-06-19 15:15:58 +0200

    copied say number code

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 'use strict';
 
+process.env.DEBUG = 'actions-on-google:*';
+
 const ActionsSdkApp = require('actions-on-google').ActionsSdkApp;
 
-exports.<insertCloudFunctionName>= (req, res) => {
-  const app = new ActionsSdkApp({request: req, response: res});
+exports.sayNumber = (request, response) => {
+  const app = new ActionsSdkApp({request, response});
 
-  // Create functions to handle requests here
+  function mainIntent (app) {
+    console.log('mainIntent');
+    let inputPrompt = app.buildInputPrompt(true, '<speak>Hi! <break time="1"/> ' +
+      'I can read out an ordinal like ' +
+      '<say-as interpret-as="ordinal">123</say-as>. Say a number.</speak>',
+      ['I didn\'t hear a number', 'If you\'re still there, what\'s the number?', 'What is the number?']);
+    app.ask(inputPrompt);
+  }
 
-}
+  function rawInput (app) {
+    console.log('rawInput');
+    if (app.getRawInput() === 'bye') {
+      app.tell('Goodbye!');
+    } else {
+      let inputPrompt = app.buildInputPrompt(true, '<speak>You said, <say-as interpret-as="ordinal">'
+        + app.getRawInput() + '</say-as></speak>',
+        ['I didn\'t hear a number', 'If you\'re still there, what\'s the number?', 'What is the number?']);
+      app.ask(inputPrompt);
+    }
+  }
+
+  let actionMap = new Map();
+  actionMap.set(app.StandardIntents.MAIN, mainIntent);
+  actionMap.set(app.StandardIntents.TEXT, rawInput);
+
+  app.handleRequest(actionMap);
+};
